5988a4c0147cca3fc7601c0dc25222646cf3cfe2
Sujit Nirmal <nirmalsujit541@gmail.com>
2024-11-22 10:03:32 +0530

    Update docker.py

--- ml_deploy_lite/docker.py
+++ ml_deploy_lite/docker.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ml_deploy_lite/docker.py
 
 def create_dockerfile():
@@ -17,24 +16,4 @@ def create_dockerfile():
     CMD ["gunicorn", "-b", "0.0.0.0:5000", "ml_deploy_lite.api:MLDeployLite.run"]
     """
     with open('Dockerfile', 'w') as f:
-=======
-# ml_deploy_lite/docker.py
-
-def create_dockerfile():
-    dockerfile_content = """
-    FROM python:3.11-slim
-
-    WORKDIR /app
-
-    COPY requirements.txt .
-    RUN pip install --no-cache-dir -r requirements.txt
-
-    COPY . .
-
-    EXPOSE 5000
-
-    CMD ["gunicorn", "-b", "0.0.0.0:5000", "ml_deploy_lite.api:MLDeployLite.run"]
-    """
-    with open('Dockerfile', 'w') as f:
->>>>>>> c399d30 (This library  is done on version 0.7  🐍)
         f.write(dockerfile_content)
